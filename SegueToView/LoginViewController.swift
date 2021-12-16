26a61074ee25006dcb4f2695210763941399589d
Vlad <vlad14196@gmail.com>
2021-12-16 08:54:42 +0300

    View

---
+++ SegueToView/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SegueToView
+//
+//  Created by user on 16.12.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
